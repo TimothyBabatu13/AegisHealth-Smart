a4737ce9f2f5031bfa3eff031f0c6ffe5eb83d60
TImothy Akanbi <knbtimothy@gmail.com>
2024-07-20 08:09:01 +0100

    Fixed the type error coming from context api

--- aegis-health-smart/src/utils/useContext.tsx
+++ aegis-health-smart/src/utils/useContext.tsx
@@ -1,7 +1,7 @@
 import { AppointmentTableContextType, AuthContextType } from '@/types/types';
 import { Context, useContext } from 'react'
 
-export const useContextHook = (Context: Context<AuthContextType | AppointmentTableContextType | null>) => {
+export const useContextHook = (Context: Context<AuthContextType | null>) => {
     
     const context = useContext(Context);
     if(context === null ) {
