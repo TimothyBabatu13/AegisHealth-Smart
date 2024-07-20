a5afee991ea69e047f12767480d742e2833b2ca3
TImothy Akanbi <knbtimothy@gmail.com>
2024-07-20 15:51:40 +0100

    Fixing error coming from layout of appointment page

--- aegis-health-smart/src/app/(screens)/appointment/join/[id]/layout.tsx
+++ aegis-health-smart/src/app/(screens)/appointment/join/[id]/layout.tsx
@@ -24,7 +24,7 @@ const layout = ({
             {children}
         </PageWrapper>
         <SmallPageWrapper>
-            Hello How're you?
+            Hello How&apos;re you?
         </SmallPageWrapper>
         
     </div>
