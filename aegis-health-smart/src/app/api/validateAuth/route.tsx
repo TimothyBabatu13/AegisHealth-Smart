7abdd798ea689a70523aabaa9c4b9949dd3ff0d1
TImothy Akanbi <knbtimothy@gmail.com>
2024-07-24 18:56:26 +0100

    made changes to validate auth api

--- aegis-health-smart/src/app/api/validateAuth/route.tsx
+++ aegis-health-smart/src/app/api/validateAuth/route.tsx
@@ -1,14 +1,14 @@
-import { app } from "@/config/firebaseConfig";
-import { ValidateAuth } from "@/utils/firebase"
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { app } from "@/config/firebaseConfig";
+// import { ValidateAuth } from "@/utils/firebase"
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-export const GET = async () => {
-    const res = await ValidateAuth()
+// export const GET = async () => {
+//     const res = await ValidateAuth()
 
-    return new Response(JSON.stringify(res), {
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        status: 200
-    })
-}
+//     return new Response(JSON.stringify(res), {
+//         headers: {
+//             "Content-Type" : "application/json",
+//         },
+//         status: 200
+//     })
+// }
