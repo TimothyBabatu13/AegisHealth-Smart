2f1bf05b8fed034fe730040c0b66614b7c04bff4
TImothy Akanbi <knbtimothy@gmail.com>
2024-07-25 10:31:22 +0100

    Added hover card to Appointment table page

--- aegis-health-smart/src/app/(screens)/appointment/components/TableHospital.tsx
+++ aegis-health-smart/src/app/(screens)/appointment/components/TableHospital.tsx
@@ -1,17 +1,58 @@
-'use client';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { cn } from "@/lib/utils"
+
+const Text = ({ width, address, hospital, className } : {
+  width: boolean,
+  address: string,
+  hospital: string,
+  className?: string
+}) => {
+  return (
+    <>
+      <h3 className={cn(`text-[#344054] ${width && 'w-[100px] min-[585px]:w-[201px]'} overflow-x-hidden whitespace-nowrap overflow-ellipsis text-sm font-medium leading-[20.3px] ` + className)}>{hospital}</h3>
+      <h4 className={cn(`text-[#667185] ${width && 'w-[100px] min-[585px]:w-[201px]'} overflow-x-hidden whitespace-nowrap overflow-ellipsis text-sm font-normal leading-[20.3px] ` + className)}>{address}</h4>
+    </>
+  )
+}
+
 const TableHospital = ({ hospital, address, width=false } : {
     hospital : string,
     address: string,
     width?: boolean
 }) => {
-  const handleHover = () => {
-    if(!width) return;
-    //show a tooltip that has the text on it
-  }
+  
   return (
-    <div onMouseOver={handleHover}>
-        <h3 className={`text-[#344054] ${width && 'w-[100px] min-[585px]:w-[201px]'} overflow-x-hidden whitespace-nowrap overflow-ellipsis text-sm font-medium leading-[20.3px]`}>{hospital}</h3>
-        <h4 className={`text-[#667185] ${width && 'w-[100px] min-[585px]:w-[201px]'} overflow-x-hidden whitespace-nowrap overflow-ellipsis text-sm font-normal leading-[20.3px]`}>{address}</h4>
+    <div>
+      {
+        width ? 
+        (
+        <HoverCard>
+          <HoverCardTrigger>
+            <Text 
+              width={width}
+              address={address}
+              hospital={hospital}
+            />
+          </HoverCardTrigger>
+          <HoverCardContent>
+          <h3 className={cn(`text-[#344054] text-sm font-medium leading-[20.3px] `)}>{hospital}</h3>
+          <h4 className={cn(`text-[#667185] text-sm font-normal leading-[20.3px] `)}>{address}</h4>
+          </HoverCardContent>
+        </HoverCard>
+        ) 
+        :  
+        (
+          <Text 
+            hospital={hospital}
+            address={address}
+            width={width}
+          />
+        )
+      }
     </div>
   )
 }
